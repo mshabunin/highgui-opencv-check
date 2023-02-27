f67debb985306bdb322cb66ccc8f71a319e90147
Maksim Shabunin <maksim.shabunin@gmail.com>
2023-02-28 01:27:58 +0300

    Enabled OpenGL for GTK2, added X11 forwarding

--- run.sh
+++ run.sh
@@ -2,8 +2,14 @@
 
 set -ex
 
-PLATFORMS=(win32 qt gtk2 gtk3)
+PLATFORMS=(qt)
+# PLATFORMS=(qt gtk2 gtk3)
 # PLATFORMS=(win32)
+
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+chmod 755 $XAUTH
 
 for p in ${PLATFORMS[@]} ; do
     echo "===" ${p} "==="
@@ -11,10 +17,16 @@ for p in ${PLATFORMS[@]} ; do
     docker build -t ${TAG} - < Dockerfile-${p}
     docker run -it \
         -v `pwd`/../opencv:/opencv \
-        -v `pwd`/../opencv_contrib:/opencv_contrib \
+        -v `pwd`/../opencv_contrib:/opencv_contrib:ro \
+        -v `pwd`/../opencv_extra:/opencv_extra:ro \
         -v `pwd`/scripts:/scripts:ro \
         -v `pwd`/workspace:/workspace \
         -u $(id -u):$(id -g) \
         -e CCACHE_DIR=/workspace/.ccache \
+        --net=host \
+        -v $XSOCK -v $XAUTH \
+        -e XAUTHORITY=$XAUTH \
+        -e DISPLAY=$DISPLAY \
+        -e OPENCV_TEST_DATA_PATH=/opencv_extra \
         ${TAG} bash /scripts/build.sh ${p}
 done
--- scripts/build.sh
+++ scripts/build.sh
@@ -7,12 +7,14 @@ d=/workspace/build-${p}
 opt=
 
 [ "${p}" == "qt" ] && opt=-DWITH_QT=ON
-[ "${p}" == "gtk2" ] && opt=-DWITH_GTK=ON
+[ "${p}" == "gtk2" ] && opt="-DWITH_GTK=ON -DWITH_OPENGL=ON"
 [ "${p}" == "gtk3" ] && opt=-DWITH_GTK=ON
 [ "${p}" == "win32" ] && opt="-DCMAKE_TOOLCHAIN_FILE=/scripts/mingw-w64-x86_64.cmake -DWITH_OPENEXR=OFF -DWITH_PROTOBUF=OFF"
 
 mkdir -p ${d}
 pushd ${d} && rm -rf *
-cmake -GNinja -DOPENCV_EXTRA_MODULES_PATH=/opencv_contrib/modules ${opt} /opencv
-ninja
+# cmake -GNinja -DOPENCV_EXTRA_MODULES_PATH=/opencv_contrib/modules ${opt} /opencv
+cmake -GNinja ${opt} /opencv
+ninja opencv_test_highgui
+./bin/opencv_test_highgui
 popd
